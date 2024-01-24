66882d13b9419fe3f0b12e2f20636b89be332508
Ronaldo Vecchi <rvecchi@gmail.com>
2024-01-24 16:57:13 -0500

    updates

--- terraform/projects/latest/latest_resource_group_dev_drinks.tf
+++ terraform/projects/latest/latest_resource_group_dev_drinks.tf
@@ -43,6 +43,7 @@ resource "azurerm_role_assignment" "latest_project_dev_drinks_resource_group_blo
 
 resource "azurerm_aadb2c_directory" "dev_drinks_b2c_directory" {
   resource_group_name     = azurerm_resource_group.latest_project_dev_drinks_resource_group.name
+  display_name            = "Dev Drinks B2C Diectory"
   country_code            = "US"
   data_residency_location = "United States"
   domain_name             = "dev${var.sub}drinks.onmicrosoft.com"
