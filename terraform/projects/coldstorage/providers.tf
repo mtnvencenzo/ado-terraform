89580befbc5a35e6f0ec73a784b0aab37d1fd333
Ronaldo Vecchi <rvecchi@gmail.com>
2024-01-26 12:34:41 +0000

    Merged PR 52: adding cold storage project
    adding cold storage project

--- terraform/projects/coldstorage/providers.tf
+++ terraform/projects/coldstorage/providers.tf
@@ -1,18 +1,18 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source = "hashicorp/azurerm"
-#       version = ">=3.86.0"
-#     }
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">=3.86.0"
+    }
 
-#     azuredevops = {
-#       source = "microsoft/azuredevops"
-#       version = ">=0.11.0"
-#     }
-#   }
-# }
+    azuredevops = {
+      source = "microsoft/azuredevops"
+      version = ">=0.11.0"
+    }
+  }
+}
 
-# data "azurerm_storage_account" "terraform_storage_account" {
-#   name                = "st${var.sub}${var.region}terraformstat001"
-#   resource_group_name = "rg-${var.sub}-${var.region}-administration-${var.sequence}"
-# }
+data "azurerm_storage_account" "terraform_storage_account" {
+  name                = "st${var.sub}${var.region}terraformstat001"
+  resource_group_name = "rg-${var.sub}-${var.region}-administration-${var.sequence}"
+}
--- terraform/projects/coldstorage/variables.tf
+++ terraform/projects/coldstorage/variables.tf
@@ -1,47 +1,47 @@
-# variable "PAT" {
-# 	type = string
-# 	default = ""
-# }
+variable "PAT" {
+	type = string
+	default = ""
+}
 
-# variable "workitem_template" {
-# 	type = string
-# 	default = "Scrum"
-# }
+variable "workitem_template" {
+	type = string
+	default = "Scrum"
+}
 
-# variable "version_control" {
-# 	type = string
-# 	default = "Git"
-# }
+variable "version_control" {
+	type = string
+	default = "Git"
+}
 
-# variable "visibility" {
-# 	type = string
-# 	default = "private"
-# }
+variable "visibility" {
+	type = string
+	default = "private"
+}
 
-# variable "devops_url" {
-# 	type = string
-# 	default = "https://dev.azure.com/vecchi"
-# }
+variable "devops_url" {
+	type = string
+	default = "https://dev.azure.com/vecchi"
+}
 
-# variable "region" {
-#   type	= string
-#   default = "eus"
-# }
+variable "region" {
+  type	= string
+  default = "eus"
+}
 
-# variable "sub" {
-#   type    = string
-#   description = "Subscription short identitifer to be used in resource naming"
-#   default = "vec"
-# }
+variable "sub" {
+  type    = string
+  description = "Subscription short identitifer to be used in resource naming"
+  default = "vec"
+}
 
-# variable "sequence" {
-#   type    = string
-#   description = "The sequence number of the resource typically starting with 001"
-#   default = "001"
-# }
+variable "sequence" {
+  type    = string
+  description = "The sequence number of the resource typically starting with 001"
+  default = "001"
+}
 
-# variable "code_reviewer_user" {
-#   type = object({
-# 	id = string
-#   })
-# }
+variable "code_reviewer_user" {
+  type = object({
+	id = string
+  })
+}
