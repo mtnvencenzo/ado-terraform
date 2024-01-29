d59d62b327938578ac3af0548112733b77c37890
Ronaldo Vecchi <rvecchi@gmail.com>
2024-01-29 11:43:05 -0500

    adding pipeline permission

--- terraform/projects/latest/latest_container_registry_global_shared.tf
+++ terraform/projects/latest/latest_container_registry_global_shared.tf
@@ -29,3 +29,12 @@ resource "azuredevops_serviceendpoint_azurecr" "app_reg_latest_project_acr" {
   azurecr_subscription_id = var.azurerm_subscription_id
   azurecr_subscription_name = var.azurerm_subscription_name
 }
+
+resource "azuredevops_pipeline_authorization" "app_reg_latest_project_acr_pipeline_authorization" {
+  project_id  = azuredevops_project.latest_project.id
+  resource_id = azuredevops_serviceendpoint_azurerm.app_reg_latest_project_acr.id
+  type        = "endpoint"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
