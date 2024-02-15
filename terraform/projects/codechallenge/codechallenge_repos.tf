8219f97fc3f1b68d38005fbc081c25e60f62518d
Ronaldo Vecchi <rvecchi@gmail.com>
2024-02-15 15:32:03 -0500

    updates for henry meds code challenge repo

--- terraform/projects/codechallenge/codechallenge_repos.tf
+++ terraform/projects/codechallenge/codechallenge_repos.tf
@@ -16,3 +16,21 @@ resource "azuredevops_git_repository" "codechallenge_project_repo_forafinancial"
     ]
   }
 }
+
+# ---------------------------
+# Setting up foraint repository
+# ---------------------------
+resource "azuredevops_git_repository" "codechallenge_project_repo_henrymeds" {
+  project_id        = azuredevops_project.codechallenge_project.id
+  name              = "Interview-HenryMeds"
+  default_branch    = "refs/heads/main"
+  initialization {
+    init_type = "Clean"
+  }
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes = [
+      initialization,
+    ]
+  }
+}
