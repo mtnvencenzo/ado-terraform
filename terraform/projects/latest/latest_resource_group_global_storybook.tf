e61613eba94ce3281fce6962c4cd160f0ea0b506
Ronaldo Vecchi <rvecchi@gmail.com>
2024-03-03 20:33:19 -0500

    updates

--- terraform/projects/latest/latest_resource_group_global_storybook.tf
+++ terraform/projects/latest/latest_resource_group_global_storybook.tf
@@ -39,30 +39,3 @@ resource "azurerm_role_assignment" "latest_project_global_storybook_resource_gro
   ]
 }
 
-resource "azurerm_role_assignment" "latest_project_global_storybook_resource_group_cnd_endpoint_contributor_role_assignment" {
-  scope                 = azurerm_resource_group.latest_project_global_storybook_resource_group.id
-  role_definition_name  = "CDN Endpoint Contributor"
-  principal_id          = azuread_service_principal.app_reg_latest_project_service_principal.id
-  lifecycle {
-    prevent_destroy = true
-  }
-  depends_on = [ 
-    azurerm_resource_group.latest_project_global_storybook_resource_group,
-    azuread_service_principal.app_reg_latest_project_service_principal,
-    azuredevops_serviceendpoint_azurerm.app_reg_latest_project_sc_sp
-  ]
-}
-
-resource "azurerm_role_assignment" "latest_project_global_storybook_resource_group_cnd_profile_contributor_role_assignment" {
-  scope                 = azurerm_resource_group.latest_project_global_storybook_resource_group.id
-  role_definition_name  = "CDN Profile Contributor"
-  principal_id          = azuread_service_principal.app_reg_latest_project_service_principal.id
-  lifecycle {
-    prevent_destroy = true
-  }
-  depends_on = [ 
-    azurerm_resource_group.latest_project_global_storybook_resource_group,
-    azuread_service_principal.app_reg_latest_project_service_principal,
-    azuredevops_serviceendpoint_azurerm.app_reg_latest_project_sc_sp
-  ]
-}
