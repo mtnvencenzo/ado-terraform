8a3a936e310d5e40eecdecf184c8148bbb04d93a
Ronaldo Vecchi <rvecchi@gmail.com>
2024-03-03 20:38:03 -0500

    updates

--- terraform/projects/latest/latest_resource_group_global_shared.tf
+++ terraform/projects/latest/latest_resource_group_global_shared.tf
@@ -38,6 +38,21 @@ resource "azurerm_role_assignment" "latest_project_global_shared_resource_group_
   ]
 }
 
+
+resource "azurerm_role_assignment" "latest_project_global_shared_resource_group_cnd_profile_contributor_role_assignment" {
+  scope                 = azurerm_resource_group.latest_project_global_shared_resource_group.id
+  role_definition_name  = "CDN Profile Contributor"
+  principal_id          = azuread_service_principal.app_reg_latest_project_service_principal.id
+  lifecycle {
+    prevent_destroy = true
+  }
+  depends_on = [ 
+    azurerm_resource_group.latest_project_global_shared_resource_group,
+    azuread_service_principal.app_reg_latest_project_service_principal,
+    azuredevops_serviceendpoint_azurerm.app_reg_latest_project_sc_sp
+  ]
+}
+
 resource "azurerm_role_assignment" "latest_project_global_shared_resource_group_cnd_endpoint_contributor_role_assignment" {
   scope                 = azurerm_resource_group.latest_project_global_shared_resource_group.id
   role_definition_name  = "CDN Endpoint Contributor"
@@ -51,17 +66,3 @@ resource "azurerm_role_assignment" "latest_project_global_shared_resource_group_
     azuredevops_serviceendpoint_azurerm.app_reg_latest_project_sc_sp
   ]
 }
-
-# resource "azurerm_role_assignment" "latest_project_global_shared_resource_group_cnd_profile_contributor_role_assignment" {
-#   scope                 = azurerm_resource_group.latest_project_global_shared_resource_group.id
-#   role_definition_name  = "CDN Profile Contributor"
-#   principal_id          = azuread_service_principal.app_reg_latest_project_service_principal.id
-#   lifecycle {
-#     prevent_destroy = true
-#   }
-#   depends_on = [ 
-#     azurerm_resource_group.latest_project_global_shared_resource_group,
-#     azuread_service_principal.app_reg_latest_project_service_principal,
-#     azuredevops_serviceendpoint_azurerm.app_reg_latest_project_sc_sp
-#   ]
-# }
