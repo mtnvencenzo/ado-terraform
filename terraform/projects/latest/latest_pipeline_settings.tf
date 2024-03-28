5ab691a2470e9b8ad759a05c1d57195e4aa97f3c
Ronaldo Vecchi <rvecchi@gmail.com>
2024-03-27 21:43:19 -0400

    updates

--- terraform/projects/latest/latest_pipeline_settings.tf
+++ terraform/projects/latest/latest_pipeline_settings.tf
@@ -1,8 +1,8 @@
 resource "azuredevops_project_pipeline_settings" "latest_project_pipeline_settings" {
   project_id = azuredevops_project.latest_project.id
-  enforce_job_scope = false
-  enforce_job_scope_for_release = false
-  enforce_referenced_repo_scoped_token = false
+  enforce_job_scope = true
+  enforce_job_scope_for_release = true
+  enforce_referenced_repo_scoped_token = true
   enforce_settable_var = true
   publish_pipeline_metadata = false
   status_badges_are_private = false
