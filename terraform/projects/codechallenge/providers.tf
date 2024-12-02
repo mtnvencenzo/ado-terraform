ab0616af8a06f5c5575be0b361df1f93f251b68f
Ronaldo Vecchi <rvecchi@gmail.com>
2024-12-02 01:35:50 +0000

    Merged PR 201: updates
    updates

--- terraform/projects/codechallenge/providers.tf
+++ terraform/projects/codechallenge/providers.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = ">=3.86.0"
+      version = "=3.86.0"
     }
 
     azuredevops = {
       source = "microsoft/azuredevops"
-      version = ">=0.11.0"
+      version = "=0.11.0"
     }
   }
 }
