cc75c65f72584eaea88c86067a70c66ee5d4385f
Ronaldo Vecchi <rvecchi@gmail.com>
2025-02-01 21:46:06 +0000

    Merged PR 283: Updates for deploy dir
    updates
    
    Related work items: #319

--- .deployment/iac/terraform/main.tf
+++ .deployment/iac/terraform/main.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "=3.86.0"
+      version = "=4.16.0"
     }
 
     azuredevops = {
       source = "microsoft/azuredevops"
-      version = "=0.11.0"
+      version = "=1.6.0"
     }
   }
 
@@ -16,8 +16,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  skip_provider_registration = true
 }
 
 provider "azuredevops" {
--- .deployment/iac/terraform/projects/coldstorage/providers.tf
+++ .deployment/iac/terraform/projects/coldstorage/providers.tf
@@ -2,12 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "=3.86.0"
     }
 
     azuredevops = {
       source = "microsoft/azuredevops"
-      version = "=0.11.0"
     }
   }
 }
