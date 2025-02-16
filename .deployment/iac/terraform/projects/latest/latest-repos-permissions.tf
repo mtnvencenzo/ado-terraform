3dd65b063bc9591762a3887908eae8c099b13c22
Ronaldo Vecchi <rvecchi@gmail.com>
2025-02-16 12:55:56 -0700

    updates

--- .deployment/iac/terraform/projects/latest/latest-repos-permissions.tf
+++ .deployment/iac/terraform/projects/latest/latest-repos-permissions.tf
@@ -2,12 +2,16 @@ data "azuredevops_users" "build_service" {
   principal_name = azuredevops_project.latest_project.id
 }
 
+# --------------------------------------------------------------------------------------------------
+# Adding permissions here because without this the build was unable to commit a git tag to the repo.
+# [GenericRead] and [CreateTag] were there by default, adding [GenericContribute]
+# --------------------------------------------------------------------------------------------------
 resource "azuredevops_git_permissions" "build_service_permission" {
   project_id    = azuredevops_project.latest_project.id
   principal     = one(data.azuredevops_users.build_service.users).descriptor
   permissions = {
     GenericContribute     = "Allow"
-    Read                  = "Allow"
+    GenericRead           = "Allow"
     CreateTag             = "Allow"
   }
 }
