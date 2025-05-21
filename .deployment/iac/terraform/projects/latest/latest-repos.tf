9cee88fcfe6853830e4207dc7bdf66bdce95f8fd
Ronaldo Vecchi <rvecchi@gmail.com>
2025-05-21 17:26:11 -0600

    updates

--- .deployment/iac/terraform/projects/latest/latest-repos.tf
+++ .deployment/iac/terraform/projects/latest/latest-repos.tf
@@ -143,20 +143,20 @@
 
 
 
-# ---------------------------
-# Setting up Terraform Mods repository
-# ---------------------------
-resource "azuredevops_git_repository" "latest_project_repo_terraform_modules" {
-  project_id     = azuredevops_project.latest_project.id
-  name           = "Terraform-Modules"
-  default_branch = "refs/heads/main"
-  initialization {
-    init_type = "Clean"
-  }
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes = [
-      initialization,
-    ]
-  }
-}
+# # ---------------------------
+# # Setting up Terraform Mods repository
+# # ---------------------------
+# resource "azuredevops_git_repository" "latest_project_repo_terraform_modules" {
+#   project_id     = azuredevops_project.latest_project.id
+#   name           = "Terraform-Modules"
+#   default_branch = "refs/heads/main"
+#   initialization {
+#     init_type = "Clean"
+#   }
+#   lifecycle {
+#     prevent_destroy = true
+#     ignore_changes = [
+#       initialization,
+#     ]
+#   }
+# }
